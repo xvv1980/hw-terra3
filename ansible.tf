445f5b1369bb7b0d688e6bb3a60125b6e0a13ca0
Vasiliy Khramov <xvv1980@yandex.ru>
2024-06-19 21:39:08 +0400

    tmp

--- ansible.tf
+++ ansible.tf
@@ -1,9 +1,9 @@
 resource "local_file" "inventory" {
-  content = templatefile("/home/xvv/hm-terra/03/hw-terra3/hosts.tftpl",
+  content = templatefile("${path.module}/hosts.tftpl",
 
   { webservers = yandex_compute_instance.web-servers,
     dbservers =  yandex_compute_instance.db-servers,
-    storages =   yandex_compute_instance.storages
+    storages =   [yandex_compute_instance.storages]
   })
 
   filename = "${abspath(path.module)}/hosts.cfg"
